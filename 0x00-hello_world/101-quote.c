ecb07d106effb7f3099ad8ce7426e40555648c15
Tevin96 <machariatevin@gmail.com>
2023-07-14 18:17:00 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-* main - A program that prints to the standard error
-* Return 1 (Success)
+* main - A program that prints a line to the standard error
+* Return 0 (Success)
 */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
