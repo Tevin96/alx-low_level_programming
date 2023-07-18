1b2d490ee7021458d9324fcf4c93cc34bfd7a1d6
Tevin96 <machariatevin@gmail.com>
2023-07-17 19:26:45 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+*main - A program that prints all possible combinations of single digit numbers
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
