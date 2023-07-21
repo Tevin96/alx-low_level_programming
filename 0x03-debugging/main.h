efe70235580c9f537acb900d68b21cf5ceff11b6
Tevin96 <machariatevin@gmail.com>
2023-07-21 13:39:33 -0700

    0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,10 @@
-#include <main.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
+
+int largest_number(int a, int b, int c);
+
+#endif /* MAIN_H */
