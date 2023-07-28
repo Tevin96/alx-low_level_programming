c49b221addc643c98b6611f76afc3e04b516720b
Tevin96 <machariatevin@gmail.com>
2023-07-28 12:35:18 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: bytes
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, dest_a = 0;
+
+while (dest[i++])
+	dest_a++;
+{
+for (i = 0; src[i] && i < n; i++)
+	dest[dest_a++] = src[i];
+}
+return (dest);
+}
