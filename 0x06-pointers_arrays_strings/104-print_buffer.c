41b77e217cccc82c9ebcfa26929f45c86b817b21
Tevin96 <machariatevin@gmail.com>
2023-07-28 20:02:06 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_buffer -  a function that prints a buffer
+ * @b: buffer
+ * @size: bytes
+ * Return: 0 (Success)
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (i < size)
+	{
+		j = size - 0 < 10 ? size - 0 : 10;
+		printf(("%08x: ", i);
+return (0);
+}
